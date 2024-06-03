6be632fc803d594d54c164d7f562d7a04e32005d
Abhishek Gupta <Abhishek18895gupta@gmail.com>
2024-06-03 16:12:31 +0530

    data transformation

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -1,8 +1,39 @@
+
+
+# @dataclass
+# class DataIngestionConfig:
+#     train_data_path: str=os.path.join('artifacts',"train.csv")
+#     test_data_path: str=os.path.join('artifacts',"test.csv")
+#     raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+# class DataIngestion:
+#     def __init__(self):
+#         self.ingestion_config=DataIngestionConfig()
+
+#     def initiate_data_ingestion(self): # Moving the data to the database is called initiating it.
+#         try:
+#             #reading the data from Mysql
+#             df=pd.read_csv(os.path.join('D:\MLPROJECTS\notebook\data','data.csv'))
+#             logging.info("Reading completed  Mysql database")
+
+#             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+#             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+#             train_set,test_set= train_test_split(df, test_size=0.2, random_state=42)
+#             df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+#             df.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+
+#         except Exception as e:
+#             logging.info("Data initialization has started")
+#             raise CustomException(e, sys)
+
+
 import os
 import sys
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
-import pandas
+import pandas as pd
 from dataclasses import dataclass
 from src.mlproject.utils import read_sql_data
 from sklearn.model_selection import train_test_split
@@ -15,22 +46,27 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()
+        self.ingestion_config = DataIngestionConfig()
 
-    def initiate_data_ingestion(self): # Moving the data to the database is called initiating it.
+    def initiate_data_ingestion(self):
         try:
-            #reading the data from Mysql
-            df=read_sql_data()
-            logging.info("Reading completed  Mysql database")
+            # Use raw string or replace backslashes with forward slashes
+            data_path = r'D:\MLPROJECTS\notebook\data\data.csv'
+        
+            
+            df = pd.read_csv(data_path)
+            logging.info("Reading completed from the data file")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            train_set,test_set= train_test_split(df, test_size=0.2, random_state=42)
-            df.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            df.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
+            # Return the paths to be unpacked in the main script
+            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
 
         except Exception as e:
-            logging.info("Data initialization has started")
+            logging.info("Data initialization has failed")
             raise CustomException(e, sys)
