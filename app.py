b9c51fe7860c952ad3f43e473457d8156f106e88
Abhishek Gupta <Abhishek18895gupta@gmail.com>
2024-06-06 16:27:01 +0530

    Flask app

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-import os
-import sys
-from src.mlproject.logger import logging
-from src.mlproject.exception import CustomException
-from src.mlproject.components.data_ingestion import DataIngestion
-from src.mlproject.components.data_ingestion import DataIngestionConfig
-from src.mlproject.components.data_transformation import DataTransformation
-from src.mlproject.components.model_trainer import ModelTrainer, ModelTrainerConfig
+from flask import Flask, render_template, request
+from src.mlproject.pipeline.predict_pipeline import CustomData, PredictPipeline
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        try:
+            # Extract data from the form
+            data = CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('writing_score')),
+                writing_score=float(request.form.get('reading_score'))
+            )
+            # Prepare data for prediction
+            pred_df = data.get_data_as_data_frame()
+            
+            # Make prediction
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            
+            return render_template('home.html', results=results[0])
+        except Exception as e:
+            # Handle errors gracefully
+            error_message = f"An error occurred: {str(e)}"
+            return render_template('home.html', error_message=error_message)
 
 if __name__ == "__main__":
-    logging.info("The Execution has started")
-
-    try:
-        data_ingestion_config = DataIngestionConfig()
-        
-        data_ingestion = DataIngestion()
-        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
-
-        data_transformation = DataTransformation()
-        train_arr, test_arr, _=data_transformation.initiate_data_transformation(train_data_path, test_data_path)
-
-        # Model Training 
-        model_trainer = ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
-
-
-    except Exception as e:
-        logging.info('Custom Exception')
-        raise CustomException(e, sys)
+    app.run(host="0.0.0.0", debug=True)
--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -1,17 +1,65 @@
+# import os
+# import sys
+# from mlproject.logger import logging
+# from mlproject.exception import CustomException
+# import pandas as pd
+# from dataclasses import dataclass
+# from src.mlproject.utils import read_sql_data
+# from sklearn.model_selection import train_test_split
+# from src.mlproject.components.model_trainer import ModelTrainer
+# from src.mlproject.components.data_transformation import DataTransformation
+
+# @dataclass
+# class DataIngestionConfig:
+#     train_data_path: str=os.path.join('artifacts',"train.csv")
+#     test_data_path: str=os.path.join('artifacts',"test.csv")
+#     raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+# class DataIngestion:
+#     def __init__(self):
+#         self.ingestion_config = DataIngestionConfig()
+
+#     def initiate_data_ingestion(self):
+#         try:
+#             # Use raw string or replace backslashes with forward slashes
+#             data_path = r'D:\MLPROJECTS\notebook\data\data.csv'
+        
+            
+#             df = pd.read_csv(data_path)
+#             logging.info("Reading completed from the data file")
+
+#             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+#             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+#             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+#             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+#             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+#             # Return the paths to be unpacked in the main script
+#             return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+
+#         except Exception as e:
+#             logging.info("Data initialization has failed")
+#             raise CustomException(e, sys)
+
+
+
 import os
 import sys
-from src.mlproject.logger import logging
-from src.mlproject.exception import CustomException
+from ..logger import logging
+from ..exception import CustomException
 import pandas as pd
 from dataclasses import dataclass
-from src.mlproject.utils import read_sql_data
+from ..utils import read_sql_data
 from sklearn.model_selection import train_test_split
+from .model_trainer import ModelTrainer
+from .data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -21,7 +69,6 @@ class DataIngestion:
         try:
             # Use raw string or replace backslashes with forward slashes
             data_path = r'D:\MLPROJECTS\notebook\data\data.csv'
-        
             
             df = pd.read_csv(data_path)
             logging.info("Reading completed from the data file")
@@ -39,3 +86,4 @@ class DataIngestion:
         except Exception as e:
             logging.info("Data initialization has failed")
             raise CustomException(e, sys)
+
